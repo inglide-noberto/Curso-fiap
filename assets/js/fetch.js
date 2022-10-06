0b5046e88cf72ac0e72d00edb57c411953830fc6
Inglide Noberto <inglidenoberto961@gmail.com>
2022-10-06 15:40:36 -0300

    Deploy

--- assets/js/fetch.js
+++ assets/js/fetch.js
@@ -1,24 +1,60 @@
+// const cep = document.querySelector("#cep");
+
+// // o evento blur significa que clicamos na caixa de texto e saimos dela
+// cep.addEventListener("blur", async (e) => {
+//     // erro mais comum: capturar o value fora de um evento
+//     let search = cep.value.replace("-","");
+
+//     const options = {
+//         method: 'GET',
+//         mode: 'cors',
+//         cache: 'default'
+//     };
+
+//     // https://viacep.com.br/ws/01001000/json/
+
+//     // fetch vai gerar uma promise. Promise é uma promessa
+//     //se der certo, ele vai retornar o resultado, se der errado ele vai retornar um erro
+//     //se der certo nós resolvemos a promisse, se der errado nos rejeitamos a promisse
+//     // Promisse é uma intenção
+//     const resultado = await fetch(`https://viacep.com.br/ws/${search}/json/`, options);
+//     const json = await resultado.json();
+
+//     console.log(json);
+// });
+
+// async / await sao primos,onde o await esta o async tem que estar
 const cep = document.querySelector("#cep");
 
-// o evento blur significa que clicamos na caixa de texto e saimos dela
-cep.addEventListener("blur", (e) => {
-    // erro mais comum: capturar o value fora de um evento
-    let search = cep.value.replace("-","");
+const showData = async (result) => {
+  for (const campo in result) {
+    if (document.querySelector("#" + campo)) {
+      document.querySelector("#" + campo).value = result[campo];
+    }
+  }
+};
 
-    const options = {
-        method: 'GET',
-        mode: 'cors',
-        cache: 'default'
-    };
+cep.addEventListener("blur", async (e) => {
+  let search = cep.value.replace("-", "");
+  console.log(Number(search));
+  if (search.length < 8 || isNaN(search)) {
+    alert("Cep inválido");
+    cep.value = "";
+    return;
+  }
 
-    // https://viacep.com.br/ws/01001000/json/
+  //cors - Cross Origin Resource Sharing
+  const chocolate = {
+    method: "GET",
+    mode: "cors",
+    cache: "default",
+  };
 
-    // fetch vai gerar uma promise. Promise é uma promessa
-    //se der certo, ele vai retornar o resultado, se der errado ele vai retornar um erro
-    //se der certo nós resolvemos a promisse, se der errado nos rejeitamos
-    fetch(`https://viacep.com.br/ws/${search}/json/`, options)
-        .then(response => {
-            response.json().then(result => console.log(result));
-        })
-        .catch((e) => console.log(e.message));
+  const resultado = await fetch(
+    `https://viacep.com.br/ws/${search}/json/`,
+    chocolate
+  );
+  const json = await resultado.json();
+
+  showData(json);
 });
